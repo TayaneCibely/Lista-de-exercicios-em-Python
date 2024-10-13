cfbb9a6effb6c963b0a5664f277cb5ab3c56857a
TayaneCibely <tayane.cibely@ufape.edu.br>
2024-10-13 16:50:55 -0300

    Atualizado com a questão 3

--- lis2_Q3.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Digite um número: "))
-b = float(input("Digite outro número: "))
-
-if a > b:
-    print("O maior número é: ", a)
-else:
-    print("O maior número é: ", b)
---
+++ list2_Q3.py
@@ -0,0 +1,9 @@
+from six import print_
+
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+if(a < b):
+    print("O menor é o primeiro: ", a)
+else:
+    print("O menor e o segundo: ", b)
